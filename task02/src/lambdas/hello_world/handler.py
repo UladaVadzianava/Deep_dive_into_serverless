a34873200b3ddb74adbe0970b18e59e310732502
Ulada_Vadzianava <ulada.vadzianava@thomsomruters.com>
2024-05-03 11:46:22 +0300

    task02

---
+++ task02/src/lambdas/hello_world/handler.py
@@ -0,0 +1,36 @@
+import json
+
+from commons.log_helper import get_logger
+from commons.abstract_lambda import AbstractLambda
+
+_LOG = get_logger('HelloWorld-handler')
+
+
+class HelloWorld(AbstractLambda):
+
+    def validate_request(self, event) -> dict:
+        pass
+
+    def handle_request(self, event, context):
+        if "rawPath" in event and event["rawPath"] == "/hello":
+            res = {
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "statusCode": 200,
+                "body": json.dumps(
+                    {
+                        "statusCode": 200,
+                        "message": "Hello from Lambda"
+                    }
+                )
+            }
+            return res
+        return 200
+
+
+HANDLER = HelloWorld()
+
+
+def lambda_handler(event, context):
+    return HANDLER.lambda_handler(event=event, context=context)
